e3e331cce963e89c0bac5a7e3f3be0baa33d56db
Sergey Galkin <svgalkin@avito.ru>
2022-09-07 10:53:52 +0300

    main: test project

---
+++ formulas/formulas_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFormulas_Sum(t *testing.T) {
+	// act
+	result := Sum(1, 2)
+
+	// assert
+	assert.Equal(t, 3, result)
+}
